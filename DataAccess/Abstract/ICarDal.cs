3b7adf455916de7bba9461e6319ccc2d396de0de
Muhammet Ali Fidan <mhmmtalifdn@gmail.com>
2023-06-09 11:09:37 +0300

    DataAccess EntityFramework Configuration

---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,11 @@
+﻿using Core.DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal : IEntityRepository<Car>
+    {
+        List<CarDetailsDto> GetCarDetails(int carId);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,34 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Contexts;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : EfEntityRepositoryBase<Car, CarRentalContext>, ICarDal
+    {
+        public List<CarDetailsDto> GetCarDetails(int carId)
+        {
+            using (CarRentalContext context = new CarRentalContext())
+            {
+                var result = from c in context.Cars
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             join co in context.Colors
+                             on c.ColorId equals co.ColorId
+                             select new CarDetailsDto()
+                             {
+                                 CarName = c.CarName,
+                                 ColorName = co.ColorName,
+                                 BrandName = b.BrandName,
+                                 DailyPrice = c.DailyPrice,
+                                 Description = c.Description,
+                                 ModelYear = c.ModelYear
+                             };
+
+                return result.ToList();
+            }
+        }
+    }
+}
